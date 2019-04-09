f6d29c02648169f108ae658aa0b02b270fec79b3
minhkhoidev <minhkhoi289@gmail.com>
2019-04-09 12:14:45 +0700

    Add footer

--- js/function.js
+++ js/function.js
@@ -33,3 +33,27 @@ $(document).ready(function(){
 	})
 });
 
+
+//slogan slide
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+var index = 1;
+currentSlide(index);
+
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  var dots = document.getElementsByClassName("dot");
+  for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";  
+  }
+  for (i = 0; i < dots.length; i++) {
+      dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";  
+  dots[slideIndex-1].className += " active";
+}
+//end slogan slide
+
