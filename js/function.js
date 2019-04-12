1158ba7eb8f99533d8992316c7b68a0522219f0f
minhkhoidev <minhkhoi289@gmail.com>
2019-04-12 11:50:37 +0700

    complete UI

--- js/function.js
+++ js/function.js
@@ -30,7 +30,10 @@ $(document).ready(function(){
 		$(this).css("background-color","white");
 		$(this).css("color","#3498db");
 		$(this).css("border-color","white");
-	})
+	});
+	
+	var index = 1;
+	currentSlide(index);
 });
 
 
@@ -39,21 +42,55 @@ function currentSlide(n) {
   showSlides(slideIndex = n);
 }
 
-var index = 1;
-currentSlide(index);
-
 function showSlides(n) {
   var i;
   var slides = document.getElementsByClassName("mySlides");
   var dots = document.getElementsByClassName("dot");
   for (i = 0; i < slides.length; i++) {
-      slides[i].style.display = "none";  
+			var classes = slides[i].className.split(" ");
+			delete classes[classes.indexOf('show')]
+			slides[i].className = classes.join(' ');
   }
   for (i = 0; i < dots.length; i++) {
       dots[i].className = dots[i].className.replace(" active", "");
   }
-  slides[slideIndex-1].style.display = "block";  
+	// slides[slideIndex-1].style.display = "block";
+	slides[slideIndex-1].className += " show";  
   dots[slideIndex-1].className += " active";
 }
 //end slogan slide
 
+//navigation
+function openNav() {
+  document.getElementById("mySidenav").style.width = "350px";
+	document.getElementByClassName("header").style.marginRight = "350px";
+	document.getElementByClassName("intro").style.marginRight = "350px";
+	document.getElementByClassName("showcase").style.marginRight = "350px";
+	document.getElementByClassName("slogan").style.marginRight = "350px";
+	document.getElementByClassName("about-team").style.marginRight = "350px";
+	document.getElementByClassName("video").style.marginRight = "350px";
+	document.getElementByClassName("some-projects").style.marginRight = "350px";
+	document.getElementByClassName("work-with").style.marginRight = "350px";
+	document.getElementByClassName("review").style.marginRight = "350px";
+	document.getElementByClassName("instagram").style.marginRight = "350px";
+	document.getElementByClassName("twitter").style.marginRight = "350px";
+	document.getElementByClassName("social").style.marginRight = "350px";
+}
+
+function closeNav() {
+  document.getElementById("mySidenav").style.width = "0";
+  document.getElementByClassName("header").style.marginRight = "0";
+	document.getElementByClassName("intro").style.marginRight = "0";
+	document.getElementByClassName("showcase").style.marginRight = "0";
+	document.getElementByClassName("slogan").style.marginRight = "0";
+	document.getElementByClassName("about-team").style.marginRight = "0";
+	document.getElementByClassName("video").style.marginRight = "0";
+	document.getElementByClassName("some-projects").style.marginRight = "0";
+	document.getElementByClassName("work-with").style.marginRight = "0";
+	document.getElementByClassName("review").style.marginRight = "0";
+	document.getElementByClassName("instagram").style.marginRight = "0";
+	document.getElementByClassName("twitter").style.marginRight = "0";
+	document.getElementByClassName("social").style.marginRight = "0";
+}
+
+//end navigation
